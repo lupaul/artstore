17fb6f6f58479ec4f26596913d88fd0ded04ee61
lupaul <lupochia@gmail.com>
2015-11-16 18:55:20 +0800

    實作轉換權限按鈕

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -1,6 +1,30 @@
 class Admin::UserController < ApplicationController
+
+	layout "admin"
+
+	before_action :authenticate_user!
+	before_action :admin_required
+
 	def index
 		@user = User.all
 		
 	end
+
+	def to_admin
+		@user = User.find(params[:user_id])
+		@user.to_admin
+
+		redirect_to admin_user_index_path
+		
+	end
+
+	def to_normal
+		@user = User.find(params[:user_id])
+		@user.to_normal
+
+		redirect_to admin_user_index_path
+		
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
-    resources :user
+    resources :user do
+      post :to_admin
+      post :to_normal
+    end
   end
   resources :products
   root "products#index"
